16d9e1a42a73ffaa2a70a03a7ca0cb44f0fb5130
metkm <metinkorkmaz417@gmail.com>
2024-09-11 21:49:57 +0300

    init

---
+++ app/components/Noise.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Vector2 } from "three";
+import commonVertex from "~/shader/common.vert?raw"
+import noiseFrag from "~/shader/noise.frag?raw"
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const uniforms = {
+  resolution: { value: new Vector2(width, height) },
+  time: { value: 0 },
+}
+</script>
+
+<template>
+  <TresMesh>
+    <TresPlaneGeometry :args="[width, height]" />
+    <TresShaderMaterial
+      :vertex-shader="commonVertex"
+      :fragment-shader="noiseFrag"
+      :uniforms="uniforms"
+    />
+  </TresMesh>
+</template>
